ad1bbaee2ef204f26c295a96b5f6c25a81271ee0
Ginanyangi <georginakabasa2018@gmail.com>
2024-05-03 10:53:13 +0300

    add final touches

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,15 +34,18 @@ const firebaseConfig = {
           <>
              
         <div className="flex  flex-col items-center justify-center min-h-screen bg-white  bg-[url('./static/login.avif')] bg-cover bg-center" >
+            <br />
+            <br />
+            <p className='font-semibold text-2xl text-black text-center'>
+              Welcome to Read Realm 
+              Explore Read Realm today and discover a world of books  just for you  </p>
+          
              <button onClick={handleGoogleLogin} className="rounded bg-red-950 px-4 py-2 text-white" >
             Sign in with Google
         </button>
         <br />
         <br />
-         <p className='font-semibold text-2xl text-black text-center'>
-             Welcome to Read Realm 
-              Explore Read Realm today and discover a world of books  just for you  </p>
-          
+         
         </div>
         </>
     );
