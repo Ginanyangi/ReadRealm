5d999e942492b85bd029bcc7070014b92e27b5dc
Ginanyangi <georginakabasa2018@gmail.com>
2024-05-03 10:19:33 +0300

    modify image path

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,7 +33,7 @@ const firebaseConfig = {
     return (
           <>
              
-        <div className="flex  flex-col items-center justify-center min-h-screen bg-white  bg-[url('./static/images/login.avif')] bg-cover bg-center" >
+        <div className="flex  flex-col items-center justify-center min-h-screen bg-white  bg-[url('./static/login.avif')] bg-cover bg-center" >
              <button onClick={handleGoogleLogin} className="rounded bg-red-950 px-4 py-2 text-white" >
             Sign in with Google
         </button>
